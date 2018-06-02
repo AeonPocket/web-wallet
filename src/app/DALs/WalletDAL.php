118ae9671a772d58d8c399fe49dd78173060a692
Pushkar Anand <pushkar8723@gmail.com>
2018-06-02 17:52:05 +0530

    Using GridFS to store transfers and keyImages.

--- src/app/DALs/WalletDAL.php
+++ src/app/DALs/WalletDAL.php
@@ -10,30 +10,53 @@ namespace App\DALs;
 
 
 use App\Models\Wallet;
-use phpDocumentor\Reflection\Types\Boolean;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class WalletDAL
 {
+    private static function saveFile($fileName, $contents) {
+        $bucket = DB::selectGridFSBucket();
+        $stream = $bucket->openUploadStream($fileName);
+        fwrite($stream, $contents);
+        fclose($stream);
+        return $fileName;
+    }
+
+    private static function getFile($fileName) {
+        $bucket = DB::selectGridFSBucket();
+        $stream = $bucket->openDownloadStreamByName($fileName);
+        return stream_get_contents($stream);
+    }
+
     public static function createWallet(String $address, int $timestamp, int $bcHeight, String $transfers, String $keyImages, bool $viewOnly=null) {
         $wallet = new Wallet();
         $wallet->address = $address;
         $wallet->bcHeight = $bcHeight;
-        $wallet->transfers = $transfers;
+        $wallet->transfers = self::saveFile($address.'-transfers', $transfers);
         $wallet->createTime = $timestamp;
-        $wallet->keyImages = $keyImages;
+        $wallet->keyImages = self::saveFile($address.'-keyImages', $keyImages);
         $wallet->viewOnly = $viewOnly;
         $wallet->save();
     }
 
     public static function getWallet(String $address) {
-        return Wallet::where('address', $address)->first();
+        $wallet = Wallet::where('address', $address)->first();
+        if ($wallet->transfers[0] == 'W') {
+            $wallet->transfers = self::getFile($wallet->transfers);
+        }
+        if ($wallet->keyImages[0] == 'W') {
+            $wallet->keyImages = self::getFile($wallet->keyImages);
+        }
+        Log::debug($wallet);
+        return $wallet;
     }
 
     public static function updateWallet(Wallet $wallet, int $bcHeight, String $transfers, String $keyImages,
                                         Array $unprocessedTx=[], int $timeStamp=null, bool $reset=null){
        $wallet->setAttribute('bcHeight', $bcHeight);
-       $wallet->setAttribute('transfers', $transfers);
-       $wallet->setAttribute('keyImages', $keyImages);
+       $wallet->setAttribute('transfers', self::saveFile($wallet->address.'-transfers', $transfers));
+       $wallet->setAttribute('keyImages', self::saveFile($wallet->address.'-keyImages', $keyImages));
        $wallet->setAttribute('unprocessedTxs', $unprocessedTx);
        $wallet->setAttribute('createTime', $timeStamp ? $timeStamp : $wallet->createTime);
        $wallet->setAttribute('reset', $reset ? $reset : $wallet->reset);
